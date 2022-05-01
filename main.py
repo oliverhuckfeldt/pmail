f35949fb963c22f53577b3feb5eec13ededfdf37
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2022-05-02 00:53:34 +0200

    Simple GUI with address field, text field, send button and stub mailer class.

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python
-from tkinter import Tk, Frame
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+
+class Mailer:
+    def __init__(self):
+        pass
+
+    def send(self):
+        messagebox.showinfo('Info', 'Message send!')
+
+
+class Window(Frame):
+    def __init__(self, app):
+        Frame.__init__(self, app, pady=10, padx=10)
+        self.app = app
+
+    def build(self):
+        topbar = Frame(self)
+        footer = Frame(self)
+
+        Label(topbar, text='Adresse').pack(side=LEFT)
+        Entry(topbar).pack(side=LEFT, expand=1, fill=X)
+        topbar.pack(fill=BOTH, expand=0)
+
+        Text(self).pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=1)
+
+        Button(footer, text='Senden', command=self.app.mailer.send).pack(side=LEFT)
+        footer.pack(fill=BOTH, expand=0)
 
 
 class App(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.win = Frame()
-        self.build()
+        self.window = Window(self)
+        self.mailer = Mailer()
+        self.init()
 
-    def build(self):
-        pass
+    def init(self):
+        self.title('Pmail')
+        self.geometry('800x600')
+        self.window.build()
 
     def run(self):
         self.mainloop()
 
 
-app = App()
-app.run()
+if __name__ == '__main__':
+    app = App()
+    app.run()
